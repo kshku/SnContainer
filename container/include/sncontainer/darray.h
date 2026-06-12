24c692a75004fd7b51dba350b65398a15cf1fdd0
kshku <1211shree@gmail.com>
2026-06-12 15:32:29 +0530

    feat(darray): add darray implementation

---
+++ container/include/sncontainer/darray.h
@@ -0,0 +1,146 @@
+#pragma once
+
+#include "sncontainer/api.h"
+
+#include <sncore/defines.h>
+#include <sncore/types.h>
+
+#define SN_DARRAY_DEFAULT_CAPACITY 5
+#define SN_DARRAY_RESIZE_FACTOR 2
+
+SN_STATIC_ASSERT(sizeof(uint64_t) == sizeof(void *), "size of pointer != size of uint64_t");
+
+typedef enum SnDArrayHeader {
+    SN_DARRAY_CAPACITY,
+    SN_DARRAY_SIZE,
+    SN_DARRAY_STRIDE,
+    SN_DARRAY_ALIGN,
+    SN_DARRAY_ALLOCATOR,
+
+    SN_DARRAY_MAX_FIELDS,
+} SnDArrayHeader;
+
+SN_CONTAINER_API void *impl_sn_darray_create(
+    uint64_t capacity, uint64_t stride, uint64_t align, SnMemoryAllocator *allocator);
+
+SN_CONTAINER_API void impl_sn_darray_destroy(void *arr);
+
+SN_CONTAINER_API void impl_sn_darray_resize(void **parr, uint64_t capacity);
+
+SN_CONTAINER_API uint64_t impl_sn_darray_header(void *arr, SnDArrayHeader header);
+
+SN_CONTAINER_API void impl_sn_darray_push(void **parr, void *element);
+
+SN_CONTAINER_API void impl_sn_darray_push_at(void **parr, uint64_t index, void *element);
+
+SN_CONTAINER_API void impl_sn_darray_pop(void **parr, void *element);
+
+SN_CONTAINER_API void impl_sn_darray_pop_at(void **parr, uint64_t index, void *element);
+
+SN_CONTAINER_API void impl_sn_darray_clear(void **parr);
+
+/**
+ * @brief Create darray with given type and capacity.
+ *
+ * @param capacity The initial capacity of the array
+ * @param type Type of the element
+ * @param allocator The allocator to use (if NULL standard library's allocator is used (malloc
+ * family functions))
+ *
+ * @return The array or NULL on failure.
+ */
+#define sn_darray_create_with_capacity(capacity, type, allocator)                   \
+    (type *)impl_sn_darray_create(capacity, sizeof(type), alignof(type), allocator)
+
+/**
+ * @brief Create a darray of given type.
+ *
+ * @param type Type of the elements of the array
+ * @param allocator The allocator to use (if NULL sn_global allocator is used)
+ *
+ * @return The array or NULL on failure.
+ */
+#define sn_darray_create(type, allocator)                                       \
+    sn_darray_create_with_capacity(SN_DARRAY_DEFAULT_CAPACITY, type, allocator)
+
+/**
+ * @brief Destroy the darray.
+ *
+ * @param parr The array
+ */
+#define sn_darray_destroy(arr) impl_sn_darray_destroy(arr)
+
+/**
+ * @brief Resize the darray to given capacity.
+ *
+ * @param parr Pointer to the array
+ * @param capacity The new capacity (number of elements)
+ */
+#define sn_darray_resize(parr, capacity) impl_sn_darray_resize((void **)parr, capacity)
+
+/**
+ * @brief Get the length of the darray (number of elements).
+ *
+ * @param arr The array
+ *
+ * @return The length of the array.
+ */
+#define sn_darray_get_length(arr) impl_sn_darray_header(arr, SN_DARRAY_SIZE)
+
+/**
+ * @brief Get the current capacity of the darray.
+ *
+ * Returns the number of elements it can hold before resizing.
+ *
+ * @param arr The array
+ *
+ * @return The capacity of the array.
+ */
+#define sn_darray_get_capacity(arr) impl_sn_darray_header(arr, SN_DARRAY_CAPACITY)
+
+/**
+ * @brief Clear the darray.
+ *
+ * Just sets the length of the array to zero. The elements should be considered
+ * garbage values.
+ *
+ * @param parr Pointer to the array
+ */
+#define sn_darray_clear(parr) impl_sn_darray_clear((void **)parr)
+
+/**
+ * @brief Push the given element to the end of the array.
+ *
+ * @param parr Pointer to the array
+ * @param element The element
+ * @param res Result
+ */
+#define sn_darray_push(parr, element)                                    \
+    impl_sn_darray_push((void **)parr, (__typeof__(element)[]){element})
+
+/**
+ * @brief Pop the element from the end of the array.
+ *
+ * @param parr Pointer to the array
+ * @param element Pointer to store poped element (can be NULL)
+ */
+#define sn_darray_pop(parr, element) impl_sn_darray_pop((void **)parr, element)
+
+/**
+ * @brief Push the element to given index of the array.
+ *
+ * @param parr Pointer to the array
+ * @param index Index to insert the element to
+ * @param element The element to insert
+ */
+#define sn_darray_push_at(parr, index, element)                                    \
+    impl_sn_darray_push_at((void **)parr, index, (__typeof__(element)[]){element})
+
+/**
+ * @brief Pop the element at given index of the array.
+ *
+ * @param parr Pointer to the array
+ * @param index Index to pop the element
+ * @param element Pointer to store element (can be NULL)
+ */
+#define sn_darray_pop_at(parr, index, element) impl_sn_darray_pop_at((void **)parr, index, element)
